69e254d76e6c8a98b6f89d6d5b2ca5a1f0efe0a1
escaflowne <vlad_dip@ukr.net>
2012-03-14 00:18:45 +0200

    adding files charts and graphs

---
+++ templates/TaskSubmit/visualization.php
@@ -0,0 +1,17 @@
+
+
+<html>
+<head>
+	<title><?= Lng::get('tast-set-edit-file.mega-workflow-editor'); ?></title>
+	<base href="<?= WWW_ROOT; ?>" />
+	<link rel="stylesheet" href="css/common.css" type="text/css" />
+	<link rel="stylesheet" href="css/frontend.css" type="text/css" />
+	<script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
+
+</head>
+<body style="width: 100%; height: 100%; margin: 0; padding: 0;">
+<h1>ВИЗУАЛИЗАЦИЯ</h1>
+<?= $this->visualization; ?>
+
+</body>
+</html>
